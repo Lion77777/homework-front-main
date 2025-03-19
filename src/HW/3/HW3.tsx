63aca12d3773dc4a97e8215d21724a499d9de55a
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-03-19 19:48:13 +0400

    Display the list of texts

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -21,8 +21,6 @@ export const HW3 = () => {
   };
 
   const handleSave = () => {
-    // ЗАСЕТАТЬ БЫ ТЕКСТ В texts И НЕ ПОТЕРЯТЬ НАПУТСТВИЕ ИЗ ПРОШЛОГО ВЕКА)
-    // А ЗАТЕМ УБРАТЬ ЗА СОБОЙ В currentText
     let newTexts = [...texts, currentText];
 
     setTexts(newTexts);
@@ -34,7 +32,7 @@ export const HW3 = () => {
       {currentText ? (
        <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
-       <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
+       <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1>
       )}
 
       <input id={'hw03-input'} type="text" value={currentText} onChange={handleChange} />
@@ -43,17 +41,17 @@ export const HW3 = () => {
        Сохранить
       </button>
 
-      {/*<h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>*/}
+      <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
-      {/*<ol id={'hw03-tasks'}>*/}
-      {/*  {ОТРИСОВАТЬ МАССИВ.map((el, index) => {*/}
-      {/*    return (*/}
-      {/*      <li key={index} id={`hw03-task-${index}`}>*/}
-      {/*        {el}*/}
-      {/*      </li>*/}
-      {/*    );*/}
-      {/*  })}*/}
-      {/*</ol>*/}
+      <ol id={'hw03-tasks'}>
+       {texts.map((el, index) => {
+         return (
+           <li key={index} id={`hw03-task-${index}`}>
+             {el}
+           </li>
+         );
+       })}
+      </ol>
     </div>
   );
 };
